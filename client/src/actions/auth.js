97f7d08d8129a8c2a44d31766c869fec0e4fd5f5
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-25 00:17:42 +0530

    User Login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from '../actions/types';
 
 // Load User
@@ -37,6 +39,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     const res = await axios.post('/api/users', body, config);
 
     dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -45,5 +48,31 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     }
 
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({ type: LOGIN_FAIL });
   }
 };
